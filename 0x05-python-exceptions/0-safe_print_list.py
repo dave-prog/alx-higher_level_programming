93efe7e9dfb9d747552fb39a93034f26a9e45fdd
David Olanrewaju <davidolanrewaju538@gmail.com>
2022-10-10 15:31:17 -0700

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    while i < x:
-        try:
-            print("{}".format(my_list[i]), end="")
-        except IndexError:
-            pass
-        i += 1
-    print("")
-    return i
+    if my_list != 0:
+        i = 0
+        while i < x:
+            try:
+                print("{}".format(my_list[i]), end="")
+            except IndexError:
+                pass
+            i += 1
+            print("")
+            return i
