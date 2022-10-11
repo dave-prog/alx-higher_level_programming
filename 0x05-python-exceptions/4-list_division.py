b58d53a046def767bed5d76941e2ae18f997337b
David Olanrewaju <davidolanrewaju538@gmail.com>
2022-10-11 13:13:15 -0700

    Correction

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,5 +13,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except (IndexError):
             print("out of range")
         finally:
-            newList.append(div)
-    return newList
+            new_list.append(div)
+    return new_list
