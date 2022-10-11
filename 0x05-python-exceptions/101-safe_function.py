d6b72c46869f969caa8be04ddcc5ff4b1e0f4503
David Olanrewaju <davidolanrewaju538@gmail.com>
2022-10-11 14:01:07 -0700

    Correction and Pycodestyle

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_function(fct, *args):
     try:
         return fct(*args)
-    except (ZeroDivisionError, IndentationError, ValueError, TypeError, IndexError) as e:
-        sys.stderr.write("Exception: {}".format(e))
+    except (ZeroDivisionError, ValueError, TypeError, IndexError) as e:
+        sys.stderr.write("Exception: {}\n".format(e))
         return None
