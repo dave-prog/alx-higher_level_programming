30841d1fda57b693d7644b9231d751815f658fcc
David Olanrewaju <davidolanrewaju538@gmail.com>
2022-10-12 09:31:05 -0700

    Classes & Objects - Getters and Setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Module 4-square
+Introduces getters and setters
+"""
+
+class Square:
+    """
+    Introduces getters and setters to obtain Area of square
+    """
+    def __init__(self, size=0):
+        """Instance initialization"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Initializes getter method"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Checks the if size is of integer type and is >= 0 
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    def area(self):
+        """
+        Computes Area of Square
+        """
+        return self.__size*self.__size
