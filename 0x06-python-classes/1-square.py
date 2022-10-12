0d5e55b12ae2bb193fe7f056ccc88b791dd71eb3
David Olanrewaju <davidolanrewaju538@gmail.com>
2022-10-12 08:41:45 -0700

    Classes and Objects II

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Defines a class with a "size" attribute
+"""
+
+
+class Square:
+    """ A Square class with a private attribute of size"""
+
+    def __init__(self, size):
+        """Function with special method __init__ and private class"""
+        self.__size = size
